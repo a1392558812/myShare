17c9fda7129d20170d50098cc335633fef7a688f
Awen <1392558812@qq.com>
2022-04-02 17:58:10 +0800

    '2022/4/2'

--- 22.日常分享-Vue/08.一个基于markdown的文档静态网站/blog/src/static/list/interview/interview-list1.js
+++ 22.日常分享-Vue/08.一个基于markdown的文档静态网站/blog/src/static/list/interview/interview-list1.js
@@ -1,14 +1,15 @@
 export default {
   name: '01.前端圣经',
   children: [
-    { name: '9.BFC.md' },
-    { name: '8.圣经.md' },
-    { name: '7.圣经.md' },
-    { name: '6.圣经.md' },
-    { name: '5.圣经.md' },
-    { name: '4.offsetW-offsetH、clientW-clientH、scrollW-scrollH区别.md' },
-    { name: '3.圣经.md' },
-    { name: '2.圣经.md' },
-    { name: '1.圣经.md' }
+    { name: '10.圣经.md' },
+    { name: '09.BFC.md' },
+    { name: '08.圣经.md' },
+    { name: '07.圣经.md' },
+    { name: '06.圣经.md' },
+    { name: '05.圣经.md' },
+    { name: '04.offsetW-offsetH、clientW-clientH、scrollW-scrollH区别.md' },
+    { name: '03.圣经.md' },
+    { name: '02.圣经.md' },
+    { name: '01.圣经.md' }
   ]
 }
--- 22.日常分享-Vue/08.一个基于markdown的文档静态网站/blog/src/static/list/miscellaneous/miscellaneous-list2.js
+++ 22.日常分享-Vue/08.一个基于markdown的文档静态网站/blog/src/static/list/miscellaneous/miscellaneous-list2.js
@@ -2,14 +2,15 @@ export default {
   name: '02.杂项整理',
   topping: true, // 是否置顶
   children: [
+    { name: '14.Proxy与Reflect的配合.md' },
     { name: '13.Watch与WatchEffect应用之区别.md' },
     { name: '12.uniapp中rpx与px互相转化.md' },
     { name: '11.vue3响应式原理杂谈.md' },
-    { name: '10.JSON不做深拷贝原因.md' },
+    { name: '10.Vue2响应式原理与修改了数组数值视图不更新.md' },
     { name: '09.观察者模式与发布订阅模式.md' },
     { name: '08.JavaScript垃圾回收机制.md' },
     { name: '07.vue和react.md' },
-    { name: '06.Vue2中修改了数组数值视图不更新.md' },
+    { name: '06.JSON不做深拷贝原因.md' },
     { name: '05.Dom可能的性能问题.md' },
     { name: '04.async可能的死锁.md' },
     { name: '03.vue.use做了什么.md' },
