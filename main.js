aa26f23bacd618de309db744cbe6d7df037a810a
Awen <1392558812@qq.com>
2023-07-31 09:34:43 +0800

    2023/7/23

--- main.js
+++ main.js
@@ -1,60 +1,61 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const targetBasePath = 'D:/我的文件/blog'
-const distBasePath = 'D:/我的文件/vue-blog/dist'
-const dirFileList = [
-  'demo-static',
-  'static',
-]
-const fileList = [
-  'bookmarks.html',
-  'favicon.ico',
-  'index.html'
-]
+const targetBasePath = 'D:/Awen/blog';
+const distBasePath = 'D:/Awen/vue-blog/dist';
+const dirFileList = ['demo-static', 'static'];
+const fileList = ['bookmarks.html', 'favicon.ico', 'index.html'];
 
 /**
  * 删除文件夹功能
  * @param  {String} url  文件路径，绝对路径
- * @return {Null}   
+ * @return {Null}
  */
-function deleteDir(url){
+function deleteDir(url) {
   var files = [];
-      
-  if( fs.existsSync(url) ) {  //判断给定的路径是否存在
-         
-      files = fs.readdirSync(url);   //返回文件和子目录的数组
-      files.forEach(function(file,index){
-          var curPath = path.join(url,file);
-              
-          if(fs.statSync(curPath).isDirectory()) { //同步读取文件夹文件，如果是文件夹，则函数回调
-              deleteDir(curPath);
-          } else {    
-              fs.unlinkSync(curPath);    //是指定文件，则删除
-          }
-              
-      });
-         
-      fs.rmdirSync(url); //清除文件夹
-  }else{
-      console.log("给定的路径不存在！");
-  }
 
+  if (fs.existsSync(url)) {
+    //判断给定的路径是否存在
+
+    files = fs.readdirSync(url); //返回文件和子目录的数组
+    files.forEach(function (file, index) {
+      var curPath = path.join(url, file);
+
+      if (fs.statSync(curPath).isDirectory()) {
+        //同步读取文件夹文件，如果是文件夹，则函数回调
+        deleteDir(curPath);
+      } else {
+        fs.unlinkSync(curPath); //是指定文件，则删除
+      }
+    });
+
+    fs.rmdirSync(url); //清除文件夹
+  } else {
+    console.log('给定的路径不存在！');
+  }
 }
 
-
-dirFileList.forEach((pathItem) =>{
-  deleteDir(`${targetBasePath}/${pathItem}`)
-  fs.cp(`${distBasePath}/${pathItem}`, `${targetBasePath}/${pathItem}`, { recursive: true }, (err) => {
-    if (err) {
-      console.error(err);
+dirFileList.forEach((pathItem) => {
+  deleteDir(`${targetBasePath}/${pathItem}`);
+  fs.cp(
+    `${distBasePath}/${pathItem}`,
+    `${targetBasePath}/${pathItem}`,
+    { recursive: true },
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
     }
-  });
-})
-fileList.forEach(pathItem =>{
-  fs.copyFile(`${distBasePath}/${pathItem}`, `${targetBasePath}/${pathItem}`,  (err) => {
-    if (err) {
-      console.error(err);
+  );
+});
+fileList.forEach((pathItem) => {
+  fs.copyFile(
+    `${distBasePath}/${pathItem}`,
+    `${targetBasePath}/${pathItem}`,
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
     }
-  });
-})
+  );
+});
