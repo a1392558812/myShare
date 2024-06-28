daa4622611516bc8546716d7025ff3cb14213387
Awen <60727751+a1392558812@users.noreply.github.com>
2024-06-28 17:17:19 +0800

    '2024/6/28'

--- 23.日常分享-公众号小程序基础/01.微信公众号小demo/config/config.js
+++ 23.日常分享-公众号小程序基础/01.微信公众号小demo/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'a1392558812a',
-    appID: 'wxc299a8ecec119ea6',
-    appsecret: '3208b6695bb88bf86f252632223ed2a5'
+    token: 'xxxxxxxxx',
+    appID: 'xxxxxxxxxx',
+    appsecret: 'xxxxxxx'
 }
