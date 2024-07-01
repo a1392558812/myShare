2c7601e578d3134e7d9a50911377c7d888816130
Awen <60727751+a1392558812@users.noreply.github.com>
2024-07-01 16:56:32 +0800

    2024/7/1

--- bat/create-menu.bat
+++ bat/create-menu.bat
@@ -7,9 +7,9 @@ echo Pause End
 rem 切换到 D:\Awen\blog 目录下
 cd /d D:\Awen\blog
 
-rem 执行 node ./main.js 命令
-echo Running node ./main.js...
-node ./main.js
+rem 执行 node ./menu-list/index.js 命令
+echo Running node ./menu-list/index.js...
+node ./menu-list/index.js
 
 echo All scripts executed.
 pause
